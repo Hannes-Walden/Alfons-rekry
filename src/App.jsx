1e64bd7d00848280b1645d79d10947761498dc79
Hannes-Walden <hannes.walden@outlook.com>
2026-09-21 13:44:58 +0300

    Valmiina luomaan kuvaajia

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,18 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+
+console.log("App.jsx toimii!");
 
 function App() {
     const [city, setCity] = useState("");
     const [cities, setCities] = useState([]);
+    const [hoveredCity, setHoveredCity] = useState(null);
+
+useEffect(() => {
+    fetch("https://api.sunrise-sunset.org/json?lat=60.1699&lng=24.9384&date_start=2026-01-01&date_end=2026-12-31")
+    .then(response => response.json())
+    .then(data => console.log(data))
+    .catch(error => console.error(error));
+}, []);
 
     function addCity() {
         if (city.trim() === "") {
@@ -36,15 +46,26 @@ function App() {
 
             <ul>
                 {cities.map((city, index) => (
-                    <li key={index}>
+                    <li key={index} className="flex items-center gap-2">
+                        <span
+                            className="city-name"
+                            onMouseEnter={() => setHoveredCity(city)}
+                            onMouseLeave={() => setHoveredCity(null)}
+                            >
                         {city}
+                        </span>
                         <button onClick={() => removeCity(index)}>Remove</button>
+                        {hoveredCity === city && (
+                            <span className="ml-4">
+                                kuvaaja kaupungille: {city}
+                            </span>
+                        )}
                     </li>
                 ))}
             </ul>
 
         </>
-    )
+    );
 
 }
 
